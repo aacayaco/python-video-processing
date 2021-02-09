6d7e0c60443f84d21cee9402d0c22e1946b2b306
Adrian Cayaco <aacayaco@gmail.com>
2021-02-09 22:45:04 +0800

    Updated arguements:
    * start time (seconds) - int
    * end time (seconds) - int
    * FPS - int

--- experiments/moviepy_test.py
+++ experiments/moviepy_test.py
@@ -1,5 +1,5 @@
 # https://github.com/Zulko/moviepy
-# use: python moviepy_test.py -i https://videoURL.mp4 -o ./test.webm
+# use: python moviepy_test.py -i https://videoURL.mp4 -s 1000 -e 1005 -o ./test.webm -fr 30
 
 from moviepy.editor import *
 import argparse
@@ -9,8 +9,10 @@ def main():
 
     # start of arguments
     parser.add_argument('-i', '--input', required=True, help='video URL required')
-
+    parser.add_argument('-s', '--start', required=True, help='start time (seconds) required')
+    parser.add_argument('-e', '--end', required=True, help='end time (seconds) required')
     parser.add_argument('-o', '--output', required=True, help='output path with file name requrired')
+    parser.add_argument('-fr', '--fps', required=True, help='FPS required')
     # end of arguments
 
     args = parser.parse_args()
@@ -25,6 +27,6 @@ if __name__ == "__main__":
     print("Loading video from URL...")
     video = VideoFileClip(video_url)
     print("Creating a subclip...")
-    video_clip = video.subclip(3,5)
+    video_clip = video.subclip(int(args.start),int(args.end))
     print("Writing the output file to:", output_path)
-    video_clip.write_videofile(output_path,fps=25)
+    video_clip.write_videofile(output_path,fps=int(args.fps))
