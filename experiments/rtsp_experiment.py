ca2da6bd1b8121ebb61b4cf711c3fafef12f4fb7
Adrian Cayaco <aacayaco@gmail.com>
2021-08-30 03:24:53 +0800

    Updated RTSP experiments.
    * Got the REST post body to work - image frame is read from memory.
    * TODO: add seek frames or timestamp from RTSP url as a parameter.

--- experiments/rtsp_experiment.py
+++ experiments/rtsp_experiment.py
@@ -3,54 +3,80 @@ import sys
 import time
 import requests
 import _thread
-import json
 import io
 from PIL import Image
- 
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import json
+
 """
     Because this reads from an RTSP source, we must keep reading the frames else we will get behind
-    in the code below, if not "isScoring", then we spin a thread to go score the image to a remote REST server
+    in the code below, if not "is_inferring", then we spin a thread to do an inferrence for the image to a remote REST server
 """
  
-minScore    = .99
-RTSPURL     = 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov'
-scoringURL  = 'http://192.168.15.200:8484/'
-timeout     = 6     # timeout to connect to the scoring URL
-isScoring   = False
-#headers = {'Content-type': 'application/octet-stream', 'Prediction-Key': 'insert prediction_key here if using Azure'}
+RTSPURL                 = 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov'
+INFERENCE_SERVER_URL    = 'http://localhost:8000/detect/'
+timeout                 = 6     # timeout to connect to the scoring URL
+is_inferring            = False
  
-def scoreImage(frame):
-    global isScoring
-    isScoring=True
+def infer_image(frame):
+    global is_inferring
+    is_inferring=True
     try:
         image = Image.fromarray(frame)
         stream = io.BytesIO()       # creating a steam object to not write to the disk
         image.save(stream, format="JPEG")
         stream.seek(0)
         img_for_post = stream.read()
-        score = requests.post(scoringURL, files={'file': img_for_post}, timeout=timeout).json()
-        if score["score"] >= minScore and score["label"] != 'Negative':
-            filename = str(int(time.time() * 1000))
-            image.save(filename + ".jpg")
-            file = open(filename + ".txt","w") 
-            file.write(json.dumps(score))
-            file.close() 
-            print(score)
+        
+        multipart_data = MultipartEncoder(
+            fields={
+                'file_list': (
+                    'a_frame.JPEG', # create a pseudo file from memory
+                    img_for_post,
+                ),
+                'model_name': (None, 'yolov5s'),
+                'img_size': (None, '640'),
+                'download_image': (None, 'false'),
+            }
+        )
+        
+        headers = {
+            'Content-Type': multipart_data.content_type
+        }
+        
+        response = requests.post(INFERENCE_SERVER_URL, headers=headers, data=multipart_data, timeout=timeout).json()
+        print(response) #do something with the response from the inference server
     except:
         e = sys.exc_info()[0]
-        print("Unexpected error with scoring: %s" % e)
-    isScoring=False
- 
-while True:         # outer loop to catch errors
+        print("Unexpected error with the inference: %s" % e)
+    is_inferring=False
+
+#process only X frames
+limit = 20
+processing_count = 0
+running = True
+while running:         # outer loop to catch errors
     try:
         video = cv2.VideoCapture(RTSPURL)
         while True: # inner loop for each frame
             ret, frame = video.read()
-            if ret and isScoring == False:
-                _thread.start_new_thread(scoreImage, (frame, ))
+            if ret and is_inferring == False and processing_count < limit:
+                _thread.start_new_thread(infer_image, (frame, ))
+                processing_count += 1
+            elif processing_count >= limit:
+                # stop the stream when processing limit is reached
+                video.release()
+                running = False
+                break
             elif ret == False:
+                # TODO: add an exit clause here since this will retry the RTSP when any error occurs
                 video.release()
                 video = cv2.VideoCapture(RTSPURL)
+            # elif frame is not None:
+                # TODO: confirm if this is the skipped frame
+                # print("Skipping frame received, currently busy...")
     except:
         e = sys.exc_info()[0]
         print("Unexpected error with prediction: %s" % e)
+        
+print("RTSP test done!")
